91865c422d132e4b04ed06c9c48f48a1fc6dc641
Da-moi <1000@home.nl>
2014-03-19 17:04:55 +0100

    Updates

--- template/html/com_virtuemart/user/edit_address.php
+++ template/html/com_virtuemart/user/edit_address.php
@@ -231,7 +231,7 @@ else {
 		</div>
 		<div class="uk-width-medium-1-3">
 			<div class="uk-panel uk-panel-box">
-			<?php echo shopFunctionsF::getLoginForm(TRUE, FALSE, $url); ?>
+			<?php echo shopFunctionsF::getLoginForm(FALSE, FALSE, $url); ?>
 			</div>
 		</div>
 	</div>
